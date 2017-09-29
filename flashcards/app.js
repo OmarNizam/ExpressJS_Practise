e251f205399ee7862b6c0913c226db6e1d652030
OmarNizam <eng.omarnizam@gmail.com>
2017-09-29 15:33:26 +0200

    Creating a Route with Express

--- flashcards/app.js
+++ flashcards/app.js
@@ -1,1 +1,9 @@
-const express = require('express');
+const express = require('express'); // require the app
+
+const app = express(); // create the app
+
+app.get('/', (req, res) => {
+   res.send('Hello Omar!!');
+});
+
+app.listen(3000); // app will be listening to port 3000
