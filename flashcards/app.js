22b730136ca3d11b597ca020aed3afb30ce0f423
OmarNizam <eng.omarnizam@gmail.com>
2017-09-29 15:49:27 +0200

    Adding Multiple Routes to the App

--- flashcards/app.js
+++ flashcards/app.js
@@ -3,7 +3,13 @@ const express = require('express'); // require the app
 const app = express(); // create the app
 
 app.get('/', (req, res) => {
-   res.send('Hello Omar!!');
+   res.send('<h1>I love ExpressJS !!</h1>');
 });
 
-app.listen(3000); // app will be listening to port 3000
+app.get('/hello', (req, res) => {
+   res.send('<h1>Hello Omar!!</h1>');
+});
+
+app.listen(3000, () => {
+  console.log('The application is running on localhost:3000');
+}); // app will be listening to port 3000
