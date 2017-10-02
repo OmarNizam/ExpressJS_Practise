54f220319952dc1d1864d23957a6e771585f7531
OmarNizam <eng.omarnizam@gmail.com>
2017-10-03 00:39:18 +0200

    Randomize the cards

--- flashcards/routes/cards.js
+++ flashcards/routes/cards.js
@@ -4,6 +4,14 @@ const router = express.Router();
 const { data } = require('../data/flashcardData.json');
 const { cards } = data; // = const cards = data.cards;
 
+// Create a new route to randomize cards
+router.get('/', (req, res) => {
+  const numberOfCards = cards.length; // I need the total number of cards we have to choose from.
+  const flashcardId = Math.floor( Math.random() * numberOfCards ); // randomize the cards
+  // redirect to random card
+  res.redirect(`/cards/${flashcardId}?side=question`);
+});
+
 router.get('/:id', (req, res) => {   // we deleted the cards from the bath here because we declared it in the app.js
   const { side } = req.query;
   const { id } = req.params;
