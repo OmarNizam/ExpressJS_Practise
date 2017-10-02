753ac9e9da5e9cc351b00554ec2309a69bd147e8
OmarNizam <eng.omarnizam@gmail.com>
2017-10-03 01:01:12 +0200

    redirect user to see the question side of the flash card by default

--- flashcards/routes/cards.js
+++ flashcards/routes/cards.js
@@ -9,12 +9,16 @@ router.get('/', (req, res) => {
   const numberOfCards = cards.length; // I need the total number of cards we have to choose from.
   const flashcardId = Math.floor( Math.random() * numberOfCards ); // randomize the cards
   // redirect to random card
-  res.redirect(`/cards/${flashcardId}?side=question`);
+  res.redirect(`/cards/${flashcardId}`);
 });
 
 router.get('/:id', (req, res) => {   // we deleted the cards from the bath here because we declared it in the app.js
   const { side } = req.query;
   const { id } = req.params;
+
+  if ( !side ) {
+    res.redirect(`/cards/${id}?side=question`); // if there is no side in the path redirect to the same card when side=question
+  }
 
   const text = cards[id][side];
   const { hint } = cards[id];
